dcd5ec1ad45a162fbd89b05fe672d5c27bcd0172
Pavel Kot <pavelkot_25@mail.ru>
2022-07-20 18:44:14 +0300

    diplom full

--- src/js/index.js
+++ src/js/index.js
@@ -133,6 +133,74 @@ localStorage.setItem('cart', JSON.stringify([...cart, card]))
 
 
 
+    //      получение данных с сервера
+
+
+    const API_PHOTO = 'https://62d55b9ad4406e5235590e85.mockapi.io/photo'
+
+ function getImage(method, url) {
+
+return fetch(url).then(response =>{
+
+    return response.json();
+}
+)        
+
+}
+
+getImage('Get', API_PHOTO)
+ .then(data => {    
+
+    console.log(data)
+    const respData = data;
+    createCards(respData)
+
+})
+ .catch(err => console.log(err))
+
+
+ 
+
+function createCards(data) {
+
+const cardsElements = document.getElementsByClassName('.card-prise')
+
+data.photo.forEach(cardElem => {
+    
+const cardsElemNew =  document.createElement('div')
+cardsElemNew.classList.add('card')
+cardsElemNew.innerHTML = `
+
+<div class="image">
+    <img src="${cardElem.url}" alt="#">
+
+    <button class="button-basket">
+        <i class="fa-solid fa-cart-shopping"></i> 
+    </button>
+
+</div>
+
+<p class="price"> 
+    ${cardElem.id} <span> p </span>
+</p>
+<p class="product-name">
+    ${cardElem.name}
+</p>
+
+
+`;
+
+cardsElements.appendChild(cardsElemNew)
+});
+
+ }
+
+
+
+
+
+
+
 
 
 
